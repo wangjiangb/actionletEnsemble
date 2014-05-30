07d1bc9f8ae96b59fd5c4b65745f0667a78dd359
Jiang Wang <wangjiangb@gmail.com>
2014-05-29 23:07:08 -0500

    Put all skeleton features into two.

--- extract_all_skeleton_features.m
+++ extract_all_skeleton_features.m
@@ -1,4 +1,29 @@
+%% read skeleton.
+clear;
+num_actions = 3;
+num_subject = 10;
+num_env = 2;
+
+skeleton_all = {};
+
 conf = configDailyAcitity;
+data_dir = conf.data_dir;
+
+for a  =1:num_actions
+    for s = 1:num_subject
+        for e=1:num_env
+            a
+            s
+            e
+            filename = fullfile(data_dir, sprintf('a%02d_s%02d_e%02d_skeleton.txt',a, s,e));
+            skeleton = readSkeleton(filename);
+            skeleton_all{a, s, e} = skeleton;
+        end
+    end
+end
+save('skeletons.mat', 'skeleton_all');
+
+%% Compute skeleton features.
 for a = 1:conf.num_actions
     for s=1:conf.num_subjects
         for e=1:conf.num_env
